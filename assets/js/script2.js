db801f9a17af89b47c896123fcf8f23200458653
Cory Benton <corybenton@gmail.com>
2023-05-17 20:24:14 -0400

    bug fix

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -7,7 +7,7 @@ const geoAPIKey = '1488c32472f1e3a9cd08ffc586e794751254f842';
 
 //displays favorite brewerires or no liked breweries
 function displayFave() {
-    brewArray = JSON.parse(localStorage.getItem("BrewArray"));
+    brewArray = JSON.parse(localStorage.getItem("BrewArray")) || [];
     if (brewArray.length == 0) {
         const faveBrewElE = document.getElementById("fave-brew");
         const columnDivE = document.createElement("div");
